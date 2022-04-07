ccc0c982400ebfcd11dba3d93f16395cc94eb9e6
Julia <riyamtechcampus@gmail.com>
2022-04-07 11:51:29 -0700

    color playing with js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+
+
+// todo change the image when  click on the imge below 
+function phones(phone){
+    img.src = phone;
+}
+
+
+
+function colors(color){
+    container.style.background = color;
+
+}
